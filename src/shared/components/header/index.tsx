ce298dc3b457e8f769e452dcde76b00af443ab43
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-22 23:19:56 -0600

    feat: add Header shared component

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -1,21 +1,44 @@
+import { Link } from 'react-router-dom';
 import './index.styles.css';
 
 type HeaderProps = {
+  isLogged?: boolean;
   onCreate?: () => void;
 };
 
 export const Header = (props: HeaderProps) => {
-  const { onCreate } = props;
+  const {
+    isLogged = false,
+    onCreate,
+  } = props;
 
   return (
-      <header className={'header'}>
-          <h1>SoymichelDev <span>Board</span></h1>
-          
-          <div>
-              <button className='btn-primary' onClick={onCreate}>
-                  Añadir Tarea
-              </button>
-          </div>
-      </header>
+    <header className={'header'}>
+      <h2>
+        <Link to={'/'}>SoymichelDev <span>Board</span></Link>
+      </h2>
+
+      <nav>
+        {isLogged ? (
+          <>
+            <button className='btn-primary' onClick={onCreate}>
+              Crear Tarea
+            </button>
+            <Link to={'/profile'}>
+              My Account
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to={'/profile'}>
+              Iniciar Sesión
+            </Link>
+            <Link to={'/profile'}>
+              Regístrate
+            </Link>
+          </>
+        )}
+      </nav>
+    </header>
   );
 };
