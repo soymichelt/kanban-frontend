9dd19ed1dad13e37d2fb213e4483caf42b96cbd9
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-22 23:29:15 -0600

    feat: implement global state to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { RouterComponent } from './routes';
+import { AuthStateProps, FormTaskStateProps, globalState, initialGlobalState } from './shared/states/global';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [auth, setAuth] = useState<AuthStateProps>(initialGlobalState.auth);
+  const [formEdit, setFormEdit] = useState<FormTaskStateProps>(initialGlobalState.formEdit);
+  const [formCreate, setFormCreate] = useState<FormTaskStateProps>(initialGlobalState.formCreate);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <globalState.Provider value={{
+      auth,
+      setAuth,
+      formEdit,
+      setFormEdit,
+      formCreate,
+      setFormCreate,
+    }}>
+      <RouterComponent />
+    </globalState.Provider>
   )
 }
 
