278904e7b97c31cf5a0b410e0d8b205490c33eff
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-22 23:21:42 -0600

    feat: add List shared component

--- src/shared/components/list/index.tsx
+++ src/shared/components/list/index.tsx
@@ -1,20 +1,19 @@
 import { Draggable, DroppableProvided } from 'react-beautiful-dnd';
 import './index.styles.css';
-import CompleteIcon from './../../../assets/complete-icon-blue.svg';
-import EditIcon from './../../../assets/edit-icon-blue.svg';
 import { ItemType } from '../../definitions/item';
+import { Item } from '../item/index';
 
-type SectionListProps = {
+type ListProps = {
   items: ItemType[];
   loading: boolean;
   onComplete: (itemId: string) => void;
   onEdit: (item: ItemType) => void;
-  completeId?: string;
+  completeId?: string | null;
   provided: DroppableProvided;
   sectionName: string;
 };
 
-export const List = (props: SectionListProps) => {
+export const List = (props: ListProps) => {
   const {
     items,
     loading,
@@ -23,62 +22,41 @@ export const List = (props: SectionListProps) => {
     completeId,
     provided,
     sectionName,
-} = props;
+  } = props;
 
   return (
     <div className='todo-container'>
-        <section
-            className={`todo ${loading ? 'todo-loading' : ''}`}
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-        >
-            <header className='todo-container-title'>
-                {sectionName}
-            </header>
-            {(items && items.length > 0) && items.map((item, index) => {
-                const completing = completeId === item.itemId;
-                return (
-                    <Draggable
-                        key={item.itemId}
-                        draggableId={item.itemId}
-                        index={index}
-                    >
-                        {(providedDrag) => (
-                            <article
-                                className={`todo-item ${item.itemState === 4 ? 'todo-completed' : ''}`}
-                                ref={providedDrag.innerRef}
-                                {...providedDrag.draggableProps}
-                                {...providedDrag.dragHandleProps}
-                            >
-                                <div className='todo-item-contenido'>
-                                    <h2>{item.itemCreator}</h2>
-                                    <p>{item.itemDescription}</p>
-                                </div>
-                                <div className='todo-item-actions'>
-                                    {item.itemState !== 5 && (
-                                        <button
-                                            className='btn'
-                                            onClick={() => onComplete(item.itemId)}
-                                            disabled={completing}
-                                        >
-                                            <img src={CompleteIcon} alt='Complete Icon' />
-                                        </button>
-                                    )}
-                                    <button
-                                        className='btn'
-                                        onClick={() => onEdit(item)}
-                                    >
-                                        <img src={EditIcon} alt='Edit Icon' />
-                                    </button>
-                                </div>
-                            </article>
-                        )}
-                    </Draggable>
-                );
-            })}
+      <section
+        className={`todo ${loading ? 'todo-loading' : ''}`}
+        ref={provided.innerRef}
+        {...provided.droppableProps}
+      >
+        <header className='todo-container-title'>
+          {sectionName}
+        </header>
+        {(items && items.length > 0) && items.map((item, index) => {
+          return (
+            <Draggable
+              key={item.itemId}
+              draggableId={item.itemId}
+              index={index}
+            >
+              {(providedDrag) => (
+                <Item
+                  key={item.itemId}
+                  data={item}
+                  onComplete={onComplete}
+                  onEdit={onEdit}
+                  provided={providedDrag}
+                  completeId={completeId}
+                />
+              )}
+            </Draggable>
+          );
+        })}
 
-            {provided.placeholder}
-        </section>
+        {provided.placeholder}
+      </section>
     </div>
   );
 };
