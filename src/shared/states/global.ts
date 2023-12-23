9beb52b5b4d77278e8456acc71f521d498a3c1e2
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-22 23:17:49 -0600

    refactor: update globalState and add schema

--- src/shared/states/global.ts
+++ src/shared/states/global.ts
@@ -1,3 +1,45 @@
 import { createContext } from 'react';
 
-export const GlobalState = createContext({});
+export type AuthStateProps = {
+  isLogged: boolean;
+  myAccount?: {
+    username: string;
+    email: string;
+    image?: string;
+  };
+};
+
+export type FormTaskStateProps = {
+  isOpen: boolean;
+  formData?: {
+    description?: string;
+    state?: number;
+    userId?: string;
+  };
+};
+
+export type GlobalStateSchema = {
+  auth: AuthStateProps,
+  setAuth: (auth: AuthStateProps) => void;
+  formEdit: FormTaskStateProps;
+  setFormEdit: (form: FormTaskStateProps) => void;
+  formCreate: FormTaskStateProps;
+  setFormCreate: (form: FormTaskStateProps) => void;
+};
+
+export const initialGlobalState: GlobalStateSchema = {
+  auth: {
+    isLogged: true,
+  },
+  setAuth: () => { },
+  formEdit: {
+    isOpen: false,
+  },
+  setFormEdit: () => { },
+  formCreate: {
+    isOpen: false,
+  },
+  setFormCreate: () => { },
+};
+
+export const globalState = createContext<GlobalStateSchema>(initialGlobalState);
