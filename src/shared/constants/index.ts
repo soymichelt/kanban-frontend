af20bf6e4f28420000a51e0b30e9cf9ce7e17b8f
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-23 20:16:17 -0600

    feat: add constants shared

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,7 @@
+export const SECTIONS_LIST= [
+  { sectionId: '0', name: 'ToDo' },
+  { sectionId: '1', name: 'Work In Progress' },
+  { sectionId: '2', name: 'Task Blocked' },
+  { sectionId: '3', name: 'QA Testing' },
+  { sectionId: '4', name: 'Done' },
+];
