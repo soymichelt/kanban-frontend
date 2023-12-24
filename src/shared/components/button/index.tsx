67d0bdf848c96397d051ff8cd66b409aafca0826
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-24 00:50:54 -0600

    feat: add fullwidth prop to button

--- src/shared/components/button/index.tsx
+++ src/shared/components/button/index.tsx
@@ -1,20 +1,27 @@
 import './index.styles.css';
 
 export type ButtonProps = {
+  className?: string;
   variant?: 'primary' | 'secondary';
+  fullwidth?: boolean;
   children: string | number | React.ReactElement | Array<React.ReactElement>;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 export const Button = (props: ButtonProps) => {
   const {
+    className = '',
     variant = 'primary',
     onClick,
-    children
+    children,
+    fullwidth = false,
   } = props;
 
   return (
-    <button className={`button button--${variant}`} onClick={onClick}>
+    <button
+      className={`button ${className} button--${variant} ${fullwidth && 'button--fullwidth'}`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
