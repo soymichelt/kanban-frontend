bc2bf135fe50cc65bebe6687491e5f6bff31950e
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-24 17:31:50 -0600

    feat: add Logo shared component

---
+++ src/shared/components/logo/index.tsx
@@ -0,0 +1,19 @@
+import './index.styles.css';
+
+export type LogoProps = {
+  className?: string;
+  size?: 'sm' | 'md' | 'lg';
+};
+
+export const Logo = (props: LogoProps) => {
+  const {
+    className,
+    size = 'md',
+  } = props;
+
+  return (
+    <h2 className={`logo ${className} logo--${size}`}>
+      SoymichelDev <span>Board</span>
+    </h2>
+  );
+};
