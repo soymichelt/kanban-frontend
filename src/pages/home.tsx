b03d67946baa83e0c6e8c1b003e7aaa9a9f62785
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-25 01:14:18 -0600

    feat: implement Page component to Home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,19 @@
 import { TasksContainer } from '../elements/tasks/containers';
 import { Header } from '../elements/header';
+import { Page } from '../shared/components/page';
+import { Typography } from '../shared/components/typography';
 
 export const Home = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1>Home</h1>
+      <Page>
+        <Typography component='h4'>
+          Tasks Board
+        </Typography>
+
         <TasksContainer />
-      </main>
+      </Page>
     </>
   );
 };
