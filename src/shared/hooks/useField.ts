19f96a236518b9d68bd2057baeefaf2f3316cb62
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-25 13:07:26 -0600

    refactor: update onChange argument

--- src/shared/hooks/useField.ts
+++ src/shared/hooks/useField.ts
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export const useField = (defaultValue?: string) => {
     const [value, setValue] = useState<string | undefined>(defaultValue);
 
-    const onChange = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const onChange = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
       setValue(e.currentTarget.value);
     };
     const reset = () => setValue(defaultValue);
