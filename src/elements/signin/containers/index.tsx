afa6904cca7230ab6f48edd18aaade3e81fe454d
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-27 18:59:53 -0600

    feat: implement signin

--- src/elements/signin/containers/index.tsx
+++ src/elements/signin/containers/index.tsx
@@ -1,12 +1,31 @@
+import { useContext } from 'react';
+import { signin } from '../../../services/users';
 import { useField } from '../../../shared/hooks/useField';
 import { Signin } from '../components';
+import { globalState } from '../../../shared/states/global';
+import { useNavigate } from 'react-router-dom';
 
 export const SigninContainer = () => {
+  const { setAuth } = useContext(globalState);
+  const navigate = useNavigate();
+
   const usernameField = useField('');
   const passwordField = useField('');
 
   const handleSigninClick = () => {
-    console.log('Signin data >>> ', usernameField.value, passwordField.value);
+    if (usernameField.isEmpty() || passwordField.isEmpty()) {
+      return;
+    }
+
+    signin(usernameField.value as string, passwordField.value as string)
+      .then(data => {
+        setAuth({
+          isLogged: true,
+          myAccount: data
+        });
+        navigate('/');
+      })
+      .catch(error => console.log(error));
   };
 
   return (
