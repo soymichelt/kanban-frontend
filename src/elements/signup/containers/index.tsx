2c4b1349cdadaabc716762e011c59dfe52ba5ab6
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-27 19:00:16 -0600

    feat: implement signup navigate

--- src/elements/signup/containers/index.tsx
+++ src/elements/signup/containers/index.tsx
@@ -1,24 +1,34 @@
 import { useNavigate } from 'react-router-dom';
-import { register } from '../../../services/users';
+import { signup } from '../../../services/users';
 import { useField } from '../../../shared/hooks/useField';
 import { Signup } from '../components';
+import { useContext } from 'react';
+import { globalState } from '../../../shared/states/global';
 
 export const SignupContainer = () => {
+  const { setAuth } = useContext(globalState);
   const navigate = useNavigate();
+
   const emailField = useField('');
   const phoneField = useField('');
   const usernameField = useField('');
   const passwordField = useField('');
 
   const handleSignupClick = () => {
-    register({
+    signup({
       email: emailField.value as string,
       phone: phoneField.value as string,
       username: usernameField.value as string,
       password: passwordField.value as string,
 
     })
-      .then(() => navigate('/signin'))
+      .then((data) => {
+        setAuth({
+          isLogged: true,
+          myAccount: data,
+        });
+        navigate('/');
+      })
       .catch(error => console.log(error));
   };
 
