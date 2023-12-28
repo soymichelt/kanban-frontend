e6bc765ed514630f14b1136291c71a08e7d1b80d
SoymichelDev <mtraatabladaa94@gmail.com>
2023-12-27 19:03:14 -0600

    feat: add Auth initial data to globalState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { useState } from 'react';
 import { RouterComponent } from './routes';
 import { AuthStateProps, FormTaskStateProps, globalState, initialGlobalState } from './shared/states/global';
+import { getAuthData, verifyAuthToken } from './services/config';
 
 const App = () => {
-  const [auth, setAuth] = useState<AuthStateProps>(initialGlobalState.auth);
+  const userIsAuthenticated = verifyAuthToken();
+  const userAuthData = getAuthData();
+  const [auth, setAuth] = useState<AuthStateProps>({
+    isLogged: userIsAuthenticated,
+    myAccount: userAuthData
+      ? {
+        token: userAuthData.token as string,
+        userId: userAuthData.userId as string,
+        username: userAuthData.username as string,
+        email: userAuthData.email as string,
+        phone: userAuthData.phone as string,
+        image: userAuthData.image as string,
+      }
+      : undefined,
+  });
   const [formEdit, setFormEdit] = useState<FormTaskStateProps>(initialGlobalState.formEdit);
   const [formCreate, setFormCreate] = useState<FormTaskStateProps>(initialGlobalState.formCreate);
   const [refreshingTasks, setRefreshingTasks] = useState<boolean>(initialGlobalState.refreshingTasks);
